40307da43e5d1fe43f7587d3f22837f4e0f61bfb
Flavio Hernan <flaviohernanfnunes@gmail.com>
2018-11-14 23:29:04 -0300

    Included base source code

---
+++ firmware/LEDCube_HC595/LEDCube_HC595.ino
@@ -0,0 +1,30 @@
+/*
+ * pins Function
+ * 
+ * D2   HC_595_ser0
+ * D3   HC_595_ser1
+ * D4   HC_595_ser2
+ * D5   HC_595_ser3
+ * D6   HC_595_ser4
+ * D7   HC_595_ser5
+ * D8   HC_595_RCLK (lacth)
+ * D9   HC_595_~OE (PWM)
+ * D10  SD_cs
+ * D11  SD_MOSI
+ * D12  SD_MISO
+ * D13  SD_sck
+ * A0   addr_0
+ * A1   addr_1
+ * A2   addr_2
+ * A3   HC_595_SRCLK (clock)
+ * 
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
